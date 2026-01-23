3bc8152a4ad995179ee78011bb45eef07f402048
Trent Chipman <trentchip@gmail.com>
2026-01-22 22:09:57 -0700

    Example c program that doesn't use multiplication

--- ExampleCFactorialProgram.c
+++ ExampleCFactorialProgram.c
@@ -1,23 +1,21 @@
-#include <stdio.h>
-//Just my example to look at and think about factorial calculation
-int main() {
-    int n, i;
-    // Use unsigned long long to handle larger factorials (up to 20!)
-    unsigned long long factorial = 1;
-
-    printf("Enter a non-negative integer: ");
-    scanf("%d", &n);
-
-    // Check for negative input
+//Heres one way that should be easily transferable to assembly
+int factorial(int n) {
     if (n < 0) {
-        printf("Error! Factorial of a negative number doesn't exist.\\n");
-    } else {
-        // 0! is 1, so the loop doesn't run for n=0, which is correct.
-        for (i = 1; i <= n; ++i) {
-            factorial *= i; // factorial = factorial * i;
-        }
-        printf("Factorial of %d = %llu\\n", n, factorial);
+        return -1;
     }
-
-    return 0;
+    if (n == 0) {
+        return 1;
+    }
+    int i = 1;
+    int result = 1;
+    int prevresult = 0;
+    while(i <= n) {
+        prevresult = result;
+        //result = oldresult * i;
+        for(int j = 1; j < i; j++) {
+            result += prevresult;
+        }
+        i++;
+    }
+    return result;
 }
