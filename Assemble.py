b9a784205e0662471f53b1bf00a65bcf38701675
Trent Chipman <trentchip@gmail.com>
2026-03-18 18:47:17 -0600

    Checks for .const or .data

--- Assemble.py
+++ Assemble.py
@@ -64,7 +64,7 @@ with open(sourceFile, 'r') as file:
                 continue
 
             #Check for section changes
-            if lineC == ".data":
+            if lineC == ".data" or lineC == ".const":
                 address = 0
                 data = 1
                 text = 0
